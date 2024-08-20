b418fcdf5b67fddfab2284ef7f552c839d34e6b9
sgomezp <sgomezp@gmail.com>
2024-08-20 19:20:52 +0200

    add person class for detection

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import cv2
 
 
 # Load yolov8 model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8m.pt')
 
-classes_of_interest = [2,5,7,9] # 2 = car 5 = bus,  7= truck, 8= traffic light
+classes_of_interest = [0,2,5,7,9] # 0 = person, 2 = car 5 = bus,  7= truck, 8= traffic light
 
 # load video
-video_path = './output_cars.mp4'
+video_path = './output_orlando.mp4'
 cap = cv2.VideoCapture(video_path)
 
 ret = True
@@ -18,13 +18,34 @@ while ret:
 
     if ret:
         # detect and track objects
-        results = model.track(source=frame, classes=classes_of_interest,persist=True)
+        results = model.track(source=frame, classes=classes_of_interest,conf= 0.60, persist=True)
+
+        # Iterate over detections to adjust labels manually
+        for result in results[0].boxes:
+            box = result.xyxy[0].cpu().numpy()
+            label = model.names[int(result.cls[0])]  # Class label
+            confidence = result.conf[0]  # Confidence score
+
+            # Position the label on the top-left corner of the box
+            x1, y1, x2, y2 = box
+            label_text = f'{label} {confidence:.2f}'
+
+            # Draw the box
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+
+            # Draw the label, adjusting position to avoid overlap
+            label_size = cv2.getTextSize(label_text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)[0]
+            label_y = int(y1) - 10 if int(y1) - 10 > 10 else int(y1) + 10
+            cv2.rectangle(
+                frame, (int(x1), label_y - label_size[1] - 10), (int(x1) + label_size[0], label_y + label_size[1] - 10),
+                (255, 0, 0), cv2.FILLED)
+            cv2.putText(frame, label_text, (int(x1), label_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         # plot results
-        frame_ = results[0].plot()
+        #frame_ = results[0].plot()
 
         # visualize
-        cv2.imshow('frame', frame_)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
