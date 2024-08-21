aeebdd10840e6acaec897d0abe55b804d5660e35
sgomezp <sgomezp@gmail.com>
2024-08-21 09:48:19 +0200

    feat: Implemented color-coded bounding boxes and labels based on detected class
    - Updated the object detection visualization to display bounding boxes and labels in different colors corresponding to each detected class.
    - Added functionality to assign unique colors for each class to enhance readability and differentiation in the output video.

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ model = YOLO('yolov8m.pt')
 
 classes_of_interest = [0,2,5,7,9] # 0 = person, 2 = car 5 = bus,  7= truck, 8= traffic light
 
+# dictionary that save the color for each label
+class_colors = {'person':(0,0,255),     # blue
+                 'car':(0, 255, 0),     # verde
+                 'bus':(255, 255, 0),   # Amarillo
+                 'truck':(255, 0, 0),   # Naranja
+                 'traffic light':(255, 165, 0)  # Rojo
+                 }
+
+
 # load video
 video_path = './output_orlando.mp4'
 cap = cv2.VideoCapture(video_path)
@@ -26,12 +35,17 @@ while ret:
             label = model.names[int(result.cls[0])]  # Class label
             confidence = result.conf[0]  # Confidence score
 
+            # obtaing the class color
+            color = class_colors.get(label,(255,255,255))
+
             # Position the label on the top-left corner of the box
             x1, y1, x2, y2 = box
             label_text = f'{label} {confidence:.2f}'
 
-            # Draw the box
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+            # Draw the box with the corresponding color
+
+
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
 
             # Draw the label, adjusting position to avoid overlap
             label_size = cv2.getTextSize(label_text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)[0]
